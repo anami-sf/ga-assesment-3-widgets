ca41dc45c25feadc41ad8e0e6174abdffc03d6dc
anami823 <anami823@gmail.com>
2019-09-20 14:33:02 -0700

    Create home view and route

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Widget
 
 # Create your views here.
+
+def home(request): 
+    widgets = Widget.objects.all( )
+
+# if request.method == ‘POST’:
+#     form = PokemonForm(reques.POST )
+#         if form.is_valid( )
+#             form.save( )
+#             return redirect(‘home’)    
+
+# else:
+#     form = PokemonForm( )
+
+#{‘form’: form} ,     
+    return render(request, 'home.html', {'widgets': widgets})
